1930b0ea524be86e7e72a6b52ddccc2b7c329e26
Daniel J. Pritchett <dpritchett@gmail.com>
2012-01-20 20:24:53 -0600

    added index page

--- app.rb
+++ app.rb
@@ -6,3 +6,9 @@ get '/call/:number' do
     Caller.new("http://#{request.host}:#{request.port.to_s}").dial(params[:number])
     "Now dialing #{params[:number]}..."
 end
+
+get '/' do
+    """
+    Welcome to No Calls Please!  See the project's <a href="https://github.com/dpritchett/no-calls-please">github</a> page for more information.
+    """
+end
